1b4f1094a9f954c8fe7f95b495c40526e61b88c7
gbottarlini <bottarlini.99@outlook.com>
2026-03-17 23:04:09 -0300

    Ejercicio 2 - Clase Vehiculo con atributo privado (encapsulamiento)

---
+++ ejercicio-02-vehiculo/Program.cs
@@ -0,0 +1,8 @@
+﻿Vehiculo v = new Vehiculo();
+v.marca = "Peugeot";
+v.modelo = "208";
+
+
+
+Console.WriteLine(v.marca);
+Console.WriteLine(v.modelo);
---
+++ ejercicio-02-vehiculo/Vehiculo.cs
@@ -0,0 +1,7 @@
+public class Vehiculo
+{
+    public string marca;
+    public string modelo;
+    private int patente;
+
+}
