da79665b8e3e9e5698733f9010d4c813eb181e11
gbottarlini <bottarlini.99@outlook.com>
2026-03-18 22:05:23 -0300

    Ejercicio 3 - Métodos para asignación de atributos privados

---
+++ ejercicio-03-articulo/Articulo.cs
@@ -0,0 +1,15 @@
+public class Articulo
+{
+    private string marca;
+    private string modelo;
+
+public void asignarMarca(string m)
+    {
+        marca = m;
+    }
+public void asignarModelo(string mod)
+    {
+        modelo = mod;
+    }
+
+}
---
+++ ejercicio-03-articulo/Program.cs
@@ -0,0 +1,3 @@
+﻿Articulo a = new Articulo();
+a.asignarMarca("Peugeot");
+a.asignarModelo("208");
